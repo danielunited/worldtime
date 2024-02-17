7a2d5371ff839aaf47d73ca15db44f4bf0b777e0
Daniel Alter <daniel.united@windowslive.com>
2024-02-17 19:01:16 +0200

    :mag: SEO metadata, homepage improvements

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="rtl">
-    <h1 class="page-title">מה השעה ב...</h1>
+  <div class="rtl container">
+    <span class="logo">Worldtime</span>
+    <h1>השעה עכשיו ב</h1>
     <div class="city-card-container">
       <div class="city-card" v-for="city in cities" :key="city.slug">
         <NuxtLink :to="`/city/${city.slug}`">
@@ -19,17 +20,62 @@ const cities = ref([]);
 onMounted(async () => {
   cities.value = await $fetch('/data.json');
 });
+
+useHead({
+  title: 'Worldtime | מחשבון המרת זמן בינלאומי | מה השעה בערים שונות בעולם',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'מצא/י את הפרשי זמנים בין ערים שונות בעולם וההפרש ביחס לישראל, כולל זמני פגישות בוחים גם לשעון כאן בארץ.',
+    },
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: 'Worldtime | מחשבון המרת זמן בינלאומי | מה השעה בערים שונות בעולם',
+    },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: 'השווה/י הפרשי זמנים עם כלי אונליין חינמי להמרת הזמן בין ערים שונות כמו ניו יורק, לונדון ופריז ביחס לישראל.',
+    },
+  ],
+});
 </script>
 
 <style scoped>
 .rtl {
-  direction: rtl; /* Ensures text and layout flow right-to-left */
-  text-align: right; /* Aligns text to the right */
+  direction: rtl;
+  text-align: right;
 }
-.page-title {
+
+.logo {
+  color: #737373;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 24px;
   text-align: center;
-  margin-top: 20px;
-  margin-bottom: 40px; /* Adjust spacing as needed */
+  margin: auto;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 48px;
+}
+h1 {
+  text-align: center;
+  font-size: 40px;
+  font-weight: 48px;
+  font-weight: 700;
+  margin-top: -2px;
+  margin-bottom: 24px;
+}
+
+h4 {
+  font-weight: 600;
 }
 .city-card {
   background-color: #f3f5f9;
