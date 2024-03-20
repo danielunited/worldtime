0781a2c68c19943a4e6bfcd77020eae69bab4d7c
Daniel Alter <daniel.united@windowslive.com>
2024-03-20 18:20:09 +0200

    :bug: Fix dynamic URLs missing from sitemap on prod

--- server/api/sitemap.ts
+++ server/api/sitemap.ts
@@ -1,11 +1,7 @@
 import { defineEventHandler } from 'h3';
-import fs from 'fs';
-import path from 'path';
+import items from '../../public/data.json';
 
 export default defineEventHandler(async (event) => {
-  const dataPath = path.resolve('public/data.json');
-  const items = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
-
   const sitemapEntries = items.map((item) => {
     const pathPrefix = item.type === 'city' ? '/city' : '/country';
     return {
