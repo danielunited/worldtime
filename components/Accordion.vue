6ddcd6673524cac84915222d9532b0b5750d216c
Daniel Alter <daniel.united@windowslive.com>
2024-03-23 13:16:22 +0200

    :lipstick: Improve accordion

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="accordion">
     <div class="accordion-title" @click="toggle">
-      <h2>{{ title }}</h2>
+      <div class="title-container">
+        <img v-if="icon" :src="icon" class="icon" />
+        <div>{{ title }}</div>
+      </div>
+      <img src="/assets/chevron-down.svg" class="chevron" :class="{ 'is-open': isOpen }" />
     </div>
     <div class="accordion-content" v-if="isOpen">
       <slot></slot>
@@ -12,6 +16,7 @@
 <script setup>
 const props = defineProps({
   title: String,
+  icon: String,
 });
 
 const isOpen = ref(false);
@@ -22,13 +27,39 @@ function toggle() {
 </script>
 
 <style scoped>
-.accordion-title h2 {
+.accordion-title {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   cursor: pointer;
   user-select: none;
+  font-weight: 600;
+  font-size: 16px;
+}
+
+.title-container {
+  display: flex;
+  align-items: center;
+}
+.icon {
+  margin-left: 8px;
+  width: 24px;
+  height: 24px;
+}
+
+.chevron.is-open {
+  transform: rotate(180deg);
 }
 
 .accordion-content {
   padding: 10px;
-  border-top: 1px solid #ccc;
+}
+
+.accordion-content > *:first-child {
+  margin-top: 0;
+}
+
+.accordion-content > *:last-child {
+  margin-bottom: 0;
 }
 </style>
